5208afc9b0c86e27094d9bcab51aceec6d3ae49b
kaijun <worktolife0829@gmail.com>
2021-09-15 10:02:04 +0800

    專案建置

---
+++ src/main/java/com/example/iThomeIronMan/IThomeIronManApplication.java
@@ -0,0 +1,13 @@
+package com.example.iThomeIronMan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IThomeIronManApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IThomeIronManApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/iThomeIronMan/IThomeIronManApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iThomeIronMan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IThomeIronManApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
